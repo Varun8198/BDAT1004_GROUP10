e4f16f714ec29498f5237e62a897b848703943db
Varunkumar Harshadbhai Patel <200558168@student.georgianc.on.ca>
2023-08-08 20:13:31 -0400

    Updated ALLOWED_HOSTS for Heroku deployment 4

--- pythonProject/settings.py
+++ pythonProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v(mf9t$fv@k@-7k-z!j315kp+#(g@&83sq)ev0b9%fi@k)e0b('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
